d408c5f6e5e1e181a3ac98f539459e61eda59c49
oltdaniel <53529846+oltdaniel@users.noreply.github.com>
2020-09-02 00:44:55 +0200

    better capture script

--- capture.py
+++ capture.py
@@ -1,23 +1,15 @@
 import os
 import time
+import json
 from urllib.parse import urlparse
 from selenium import webdriver
+import pathlib
 
-urls = [
-	'https://raw.githubusercontent.com/IT4Kids/levels/master/Solutions/Aquarium.cubi',
-	'https://raw.githubusercontent.com/IT4Kids/levels/master/Solutions/Autorennen.cubi',
-	'https://raw.githubusercontent.com/IT4Kids/levels/master/Solutions/BananenHergezaubert.cubi',
-	'https://raw.githubusercontent.com/IT4Kids/levels/master/Solutions/Buchstaben.cubi',
-	'https://raw.githubusercontent.com/IT4Kids/levels/master/Solutions/FangMich.cubi',
-	'https://raw.githubusercontent.com/IT4Kids/levels/master/Solutions/Farben.cubi',
-	'https://raw.githubusercontent.com/IT4Kids/levels/master/Solutions/FindeDasTier.cubi',
-	'https://raw.githubusercontent.com/IT4Kids/levels/master/Solutions/Labyrinth.cubi',
-	'https://raw.githubusercontent.com/IT4Kids/levels/master/Solutions/MausZumKaese.cubi',
-	'https://raw.githubusercontent.com/IT4Kids/levels/master/Solutions/Pacman.cubi',
-	'https://raw.githubusercontent.com/IT4Kids/levels/master/Solutions/Trampolin.cubi',
-	'https://raw.githubusercontent.com/IT4Kids/levels/master/Solutions/Wettrennen.cubi',
-	'https://raw.githubusercontent.com/IT4Kids/levels/master/Solutions/Zeichnen.cubi',
-]
+pathlib.Path('screenshots').mkdir(parents=True, exist_ok=True) 
+
+summary = json.loads(open('summary.json').read())
+
+urls = map(lambda level: 'https://raw.githubusercontent.com/IT4Kids/levels/master/Templates/' + str(level), [level['filename'] for level in summary if level['template_exists']])
 
 driver = webdriver.Chrome()
 
@@ -25,12 +17,12 @@ count = 0
 for url in urls:
 	a = urlparse(url)
 	base = os.path.basename(a.path)
-	driver.get('https://i.oltdaniel.at/Cubi/development?level=' + url)
+	driver.get('https://editor.it-for-kids.org/?level=' + url)
 	while driver.execute_script('return document.readyState;') != 'complete':
 		time.sleep(0.1)
 	time.sleep(1)
-	image = driver.find_element_by_id("pixi-app-container").screenshot_as_png
-	f = open(base + '.png', 'w+b')
+	image = driver.find_elements_by_css_selector('#pixi-app-container canvas')[0].screenshot_as_png
+	f = open('./screenshots/' + base + '.png', 'w+b')
 	f.write(image)
 	f.close()
 	count += 1
